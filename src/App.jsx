926cbaeec4a22c797aaaebb67fc9f3383b0c2daa
cristidrg <cristiandrg96@gmail.com>
2017-12-08 06:40:11 +0000

    Added css reset and basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 
-import './styles.css';
+import './styles.scss';
 import Note from './components/Note';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Note note="Done with a note? Use the archive " title="Make a listasasasasa" />
-        <br />
-        <br />
-        <Note takeANote />
-        <br />
-        <br />
-        <Note editMode />
+        <header className="app__header" />
+        <div className="left__column" />
+        <div className="content">
+          <Note note="Done with a note? Use the archive " title="Make a listasasasasa" />
+          <br />
+          <br />
+          <Note takeANote />
+          <br />
+          <br />
+          <Note editMode />
+        </div>
       </div>
     );
   }
